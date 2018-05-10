c76beb2caff12611869d5fbba75e056f2819c4a5
Pete Gleeson <pgleeson@atlassian.com>
2018-05-10 10:26:21 +1000

    renames package to mount wrapper

--- packages/enzyme-test-suite/test/_helpers/setupAdapters.js
+++ packages/enzyme-test-suite/test/_helpers/setupAdapters.js
@@ -1,5 +1,5 @@
 const Enzyme = require('enzyme');
 const Adapter = require('./adapter');
-const mountWrapper = require('enzyme-adapter-react-renderer');
+const mountWrapper = require('enzyme-mount-wrapper');
 
 Enzyme.configure({ adapter: new Adapter(), mountWrapper });
